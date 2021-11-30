54f465c5760abe8ca0cfe4897240ae7dbac2f23d
Matheus Felipe <64490699+MatheusFellype2016@users.noreply.github.com>
2021-11-29 22:49:18 -0300

    Update index.js

--- docs/js/index.js
+++ docs/js/index.js
@@ -1,29 +1,5 @@
 
 $(document).ready(function() {
-    // Navbar Shrink
-    $(window).on("scroll",function(){
-        if($(this).scrollTop() > 90) {
-            $(".navbar").addClass("navbar-shrink");
-        }
-        else {
-            $(".navbar").removeClass("navbar-shrink");
-        }
-    });
-    // Video Popup
-    // const videoSrc = $("#palyer-1").attr("src");
-    // $(".video-play-btn, .video-popup").on("click", function(){
-    //     if($(".video-popup").hasClass("open")){
-    //         $(".video-popup").removeClass("open");
-    //         $("#palyer-1").attr("src","");
-    //     }
-    //     else {
-    //         $(".video-popup").addClass("open");
-    //         if($("#palyer-1").attr("src")==''){
-    //             $("#palyer-1").attr("src",videoSrc);
-    //         }
-            
-    //     }
-    // });
     // Owl carousel
     $('.features-carousel').owlCarousel({
         loop: true,
